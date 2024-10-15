7e82ecf4a8e822303a9169ec8c1feb4b3f1cff86
Vinicius <vinicius.lima@outlook.pt>
2024-10-14 21:32:45 -0300

    chore: ajuste redação/termos

--- ETL/extract.py
+++ ETL/extract.py
@@ -5,19 +5,21 @@ logging.basicConfig(level=logging.INFO)
 
 # Script para conectar-se ao banco de dados SQLite e extrair registros da tabela vendas
 def extrair_dados_vendas(caminho_banco='data/vendas.db'):
-    conn = None
+    conexao = None
     vendas = []
-    try: # Conecta ao banco e realiza a consulta
-        conn = sqlite3.connect(caminho_banco)
-        cursor = conn.cursor()
+    try: 
+        # Conecta ao banco e realiza a consulta
+        conexao = sqlite3.connect(caminho_banco)
+        cursor = conexao.cursor()
         cursor.execute('SELECT * FROM vendas')
         vendas = cursor.fetchall()
         logging.info(f'{len(vendas)} Dados extraídos com sucesso.')
     except Exception:
         logging.error(f'Erro ao extrair dados: {Exception}')
-    finally: # Finaliza a conexão
-        if conn:
-            conn.close()
+    finally: 
+        # Finaliza a conexão
+        if conexao:
+            conexao.close()
     return vendas
 
 if __name__ == "__main__":
--- ETL/transform.py
+++ ETL/transform.py
@@ -4,48 +4,49 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-def transformar_dados(vendas): # Converte em DataFrame
+# Script para transformar os dados extraidos do SQLite em DataFrame
+def transformar_dados(vendas):
     df = pd.DataFrame(vendas, columns=[
-        'id', 'data_venda', 'id_produto', 'id_cliente', 
-        'quantidade', 'valor_unitario', 'valor_total', 
+        'id', 'data_venda', 'id_produto', 'id_cliente',
+        'quantidade', 'valor_unitario', 'valor_total',
         'id_vendedor', 'regiao'
     ])
 
     # Converte as datas para o formato ISO
     df['data_venda'] = pd.to_datetime(df['data_venda']).dt.strftime('%Y-%m-%d')
 
-    # Verifica dados duplicados *IGNORA O ID*
+    # Verifica e remove dados duplicados *IGNORA O ID*
     contagem_duplicados = df.duplicated(subset=[
-        'data_venda', 'id_produto', 'id_cliente', 
-        'quantidade', 'valor_unitario', 'valor_total', 
-        'id_vendedor', 'regiao'
+        'data_venda', 'id_produto', 'id_cliente', 'quantidade', 
+        'valor_unitario', 'valor_total', 'id_vendedor', 'regiao'
     ]).sum()
 
-    # Remove dados duplicados
     df.drop_duplicates(subset=[
-        'data_venda', 'id_produto', 'id_cliente', 
-        'quantidade', 'valor_unitario', 'valor_total', 
-        'id_vendedor', 'regiao'
+        'data_venda', 'id_produto', 'id_cliente', 'quantidade',
+        'valor_unitario', 'valor_total', 'id_vendedor', 'regiao'
     ], inplace=True)
 
     # Cálculo do total de vendas por dia
     df['total_vendas'] = df['valor_total'] * df['quantidade']
     vendas_por_dia = df.groupby('data_venda')['total_vendas'].sum().reset_index()
+
     logging.info(f'Total de vendas por dia:')
     logging.info(vendas_por_dia)
 
-    return df, contagem_duplicados # Df transformado 
+    return df, contagem_duplicados
 
-if __name__ == "__main__": # Conexão com o banco de dados
-    conn = sqlite3.connect('data/vendas.db')
-    cursor = conn.cursor()
+def obter_dados_do_banco():
+    conexao = sqlite3.connect('data/vendas.db')
+    cursor = conexao.cursor()
     cursor.execute('SELECT * FROM vendas')
     vendas = cursor.fetchall()
-    
-    df, duplicates_count = transformar_dados(vendas)
+    conexao.close()
+    return vendas
 
-    logging.info('DataFrame transformado:')
+if __name__ == "__main__": # Conexão com o banco de dados
+    vendas = obter_dados_do_banco()
+    df, contagem_duplicados = transformar_dados(vendas)
+
+    logging.info(f'Número de duplicatas encontradas: {contagem_duplicados}')
+    logging.info(f'DataFrame transformado:')
     print(df)
-
-    logging.info(f'Processo de transformação concluído com {duplicates_count} duplicatas encontradas.')
-    conn.close()
